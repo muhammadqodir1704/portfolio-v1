27a761d1d5f95ac83c3f46957a79bd5140bd2802
Muhammadqodir <goldenparen06@gmail.com>
2025-05-26 17:31:16 +0500

    feat: added font family v2

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Josefin Sans'],
+        special: ['Roboto']
+      },
+    },
+  },
+  plugins: [],
+}
